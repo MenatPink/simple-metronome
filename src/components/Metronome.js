da6f0eb6ab513e6927ff912ce289d1f2cdc2037e
Menat Pink <pink@Pinks-MacBook-Pro.local>
2022-03-25 12:28:06 +0000

    initial commit

---
+++ src/components/Metronome.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from 'react';
+import * as Tone from 'tone';
+import './Metronome.scss';
+
+
+const osc = new Tone.Oscillator().toDestination();
+let clickHistory = []
+
+
+
+const Metronome = () => {
+    const [bpm, setBpm] = useState(122);
+    const [playing, setPlaying] = useState(false);
+    let prevClick = 0
+
+    useEffect(() => {
+        if (playing) {
+            Tone.Transport.bpm.value = bpm;
+            console.log(Tone.Transport.bpm.value);
+            Tone.Transport.stop().start();
+            Tone.Transport.scheduleRepeat((time) => {
+                osc.start(time).stop(time + 0.1);
+            }, "4n");
+            Tone.Transport.start();
+        } else {
+            Tone.Transport.stop();
+        }
+    }, [playing, bpm]);
+
+    return (
+        <div className="d-flex flex-column metronome-container">
+            <h2 className="tempo-display">{`${bpm}bpm`}</h2>
+            <button className="tap-tempo-button" onClick={() => {
+                if (clickHistory.length > 3) clickHistory.splice(0, 1)
+                prevClick = new Date().getTime()
+                clickHistory.push(prevClick)
+                if (clickHistory.length === 4) {
+                    let firstClickTime = clickHistory[1] - clickHistory[0];
+                    let secondClickTime = clickHistory[2] - clickHistory[1];
+                    let thirdClickTime = clickHistory[3] - clickHistory[2];
+                    let averageClickTime = (firstClickTime + secondClickTime + thirdClickTime) / 4
+                    clickHistory = []
+                        setBpm(Math.round(60000/averageClickTime))
+                }
+            }}>{`Tap Tempo`}</button>
+            <div className="controls-container">
+                <button className="play-button" onClick={() => { setPlaying(!playing) }}>
+                    {playing ? 'stop' : 'start'}
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Metronome;
---
+++ src/containers/App.js
@@ -0,0 +1,14 @@
+import Metronome from '../components/Metronome';
+import './App.scss';
+
+
+
+const App = () => {
+	return (
+		<div className="background d-flex-center">
+			<Metronome/>
+		</div>
+	);
+};
+
+export default App;
